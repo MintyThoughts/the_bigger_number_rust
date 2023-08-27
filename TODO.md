0ab908ca833a58d29436b1dd7d97b6592fb34fba
MintyThoughts <mintythoughts@proton.me>
2023-08-27 11:44:57 -0400

    Now it can also be used as a cli tool using the crate "clap"

--- TODO.md
+++ TODO.md
@@ -1,4 +1,10 @@
 - [x] Make it work
-- [ ] Make it work with arguments as numbers too
-- [ ] Make it work with a TUI too
-- [ ] Make it work with a GUI too
+- [x] Make it work with arguments as numbers too
+
+
+It now uses the crate "clap" to get the arguments in a easy manner
+
+
+- [ ] Reorganize the code because it looks like a mess lol
+- [ ] Make it work with a good looking TUI too
+- [ ] Make it work with a GUI (GTK/QT) too
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,68 @@
+use clap::{arg, Parser};
 use std::io::{stdin, stdout, Write};
 
+// Clap Derive ( this is the argument parser section )
+
+/// Just select the ui and then input the numbers
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// UI to get
+    #[arg(short, long, default_value = "tui")]
+    cli_ui_tui_etc: String,
+    /// First number to process
+    #[arg(short, long, default_value_t = 1)]
+    first_number: i128,
+    /// Second number to process
+    #[arg(short, long, default_value_t = 0)]
+    second_number: i128,
+}
+
 fn main() {
-    println!("This program will help you know which number is bigger :)");
-    // TODO: Simplify "let a" and "let b"
+    // this initiates a variable that has all the arguments in it
+    let args = Args::parse();
+    // this passes the ui argument tu the ui variable
+    let ui = args.cli_ui_tui_etc.as_str();
+    //this checks the ui variable and runs the correspondent ui
+    if ui == "cli" {
+        cli(args);
+    } else if ui == "tui" {
+        simple_tui();
+    } else {
+        println!("you did not selected a correct ui option")
+    }
+}
+
+fn get_input() -> String {
+    // This code gets input from the terminal and then returns it without spaces or enters
+    let mut input = String::new();
+    let _ = stdout().flush();
+    stdin()
+        .read_line(&mut input)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+    input
+}
+fn input_to_integer(string: String) -> i128 {
+    // This converts the string variable passed into a integer
+    let number: i128 = string.parse().unwrap_or(0);
+    number
+}
+fn return_the_bigger_number(a: i128, b: i128) -> i128 {
+    // This function compares a and b and returns the bigger
+    if a >= b {
+        a
+    } else {
+        b
+    }
+}
+
+fn simple_tui() {
     let a = {
         print!("Type the first number: ");
         let a_but_string = get_input();
@@ -15,31 +75,13 @@ fn main() {
         let b_but_actually_integer = input_to_integer(b_but_string);
         b_but_actually_integer
     };
-    let awnser = return_the_bigger_number(a,b);
+    let awnser = return_the_bigger_number(a, b);
     println!("The bigger number is: {}", awnser)
-
-
 }
-
-fn get_input() -> String {
-    // This code gets input from the terminal and then returns it
-    let mut input = String::new();
-    let _=stdout().flush();
-    stdin().read_line(&mut input).expect("Did not enter a correct string");
-    if let Some('\n')=input.chars().next_back() {
-        input.pop();
-    }
-    if let Some('\r')=input.chars().next_back() {
-        input.pop();
-    }
-    input
-}
-fn input_to_integer(string: String) -> i128{
-    // This converts the string variable passed into a integer
-    let number:i128 = string.parse().unwrap_or(0);
-    number
-}
-fn return_the_bigger_number(a:i128, b:i128) -> i128 {
-    // This function compares a and b and returns the bigger
-    if a>=b { a } else { b }
+fn cli(args: Args) {
+    let number_one = args.first_number;
+    let number_two = args.second_number;
+    println!("The bigger number is: {}", {
+        return_the_bigger_number(number_one, number_two)
+    })
 }
